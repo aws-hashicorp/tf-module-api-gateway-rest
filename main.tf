31c41dbdfd0d047caa22335953880c193cf9756e
Hernan Quintero Atara <54866571+tna3ds@users.noreply.github.com>
2026-02-05 08:44:27 -0500

    Fix usage_plan_id reference for API key

--- main.tf
+++ main.tf
@@ -163,5 +163,5 @@ resource "aws_api_gateway_usage_plan_key" "usage_plan_key" {
   count         = var.create_api_key ? 1 : 0
   key_id        = aws_api_gateway_api_key.api_key[0].id
   key_type      = "API_KEY"
-  usage_plan_id = aws_api_gateway_usage_plan.api-usage-plan.id
+  usage_plan_id = aws_api_gateway_usage_plan.api-usage-plan[0].id
 }
